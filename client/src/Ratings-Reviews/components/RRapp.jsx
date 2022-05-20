9cf1219413331fa912f549ae1ef240737bbaf55b
Benjamin Thornton <benjamin.l.thornton@gmail.com>
2022-05-20 15:51:53 -0500

    added ratings-review folder

---
+++ client/src/Ratings-Reviews/components/RRapp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RatingSummary from './RatingSummary.jsx';
+
+export default function RatingReviews() {
+  return (
+    <>
+      <h3>Ratings and Reviews</h3>
+      <RatingSummary />
+      <div>ProductBreakdown</div>
+      <div>SelectParam</div>
+      <div>ReviewsList</div>
+    </>
+  );
+}
---
+++ client/src/Ratings-Reviews/components/RatingSummary.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RatingSummary() {
+  return (
+    <div>Ratings summary placeholder</div>
+  );
+}
