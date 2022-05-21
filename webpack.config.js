b70efbe2d89763adf33ba24f276baa8a2aae7418
valpizzo <valpizzo2@gmail.com>
2022-05-21 18:36:45 -0400

    Fixed webpack so .jsx files can be imported without file extension (to please airbnb style guide) Added widget specific folders to src (RIOC and files inside) I did not commit index.jsx fyi

--- webpack.config.js
+++ webpack.config.js
@@ -49,5 +49,8 @@ module.exports = {
     compress: true,
     // [port] what port on our local machine to run the dev server
     port: 3000,
-  }
-}
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+};
