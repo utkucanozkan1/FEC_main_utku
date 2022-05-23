695683017f7929fae078b3f392b3cf9a57202181
valpizzo <valpizzo2@gmail.com>
2022-05-23 11:09:56 -0400

    Updated index.jsx Created a folder for all Related Items / Outfit Creation components within src

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 // Bring React in to build a component.
 import React from 'react';
-import QuestionsAndAnswers from './q-a/QuestionAndAnswers.jsx';
+import QuestionsAndAnswers from './q-a/QuestionAndAnswers';
+import RelatedOutfitView from './RIOC/RelatedOutfitView';
+import RatingReviews from './Ratings-Reviews/components/RRapp';
 // Import from react-dom the ability to create a root render
 import { createRoot } from 'react-dom/client';
 // create the root of the app by selection where the app should be mounted in the dom
@@ -10,16 +12,15 @@ const root = createRoot(document.getElementById('root'));
 // const App = React.createElement("h1",null,"Hello World");
 // root.render(<App />); -> will NOT render!! root.render(App); -> will
 
-class App extends React.Component {
-  render () {
-    return (
-    <>
-    <h1> Hello World</h1>
-    <QuestionsAndAnswers></QuestionsAndAnswers>
-    </>
-    )
-  }
-};
+function App() {
+  return (
+    <div>
+      <RelatedOutfitView />
+      <RatingReviews />
+      <QuestionsAndAnswers />
+    </div>
+  );
+}
 
 // render the root element with the provided component
 root.render(<App />);
