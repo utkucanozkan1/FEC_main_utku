f66b3c3f588059968f0fa49fda048d5146338e6a
Benjamin Thornton <benjamin.l.thornton@gmail.com>
2022-05-23 10:08:39 -0500

    Changing files to styled components

--- client/src/Ratings-Reviews/RRcomponents/RRapp.jsx
+++ client/src/Ratings-Reviews/RRcomponents/RRapp.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import RatingSummary from './RatingSummary/RatingSummary.jsx';
 import ProductBreakdown from './ProductBreakdown/ProductBreakdown.jsx';
 import ReviewList from './ReviewList/ReviewList.jsx';
 
+const RatingReview = styled.section`
+display: flex;
+flex-flow: column wrap;
+align-items: center;
+`;
+
 export default function RatingReviews() {
   return (
-    <section className="RRsection">
+    <>
       <h3>Ratings and Reviews</h3>
-      <RatingSummary />
-      <ProductBreakdown />
-      <ReviewList />
-    </section>
+      <RatingReview className="RRsection">
+        <RatingSummary />
+        <ProductBreakdown />
+        <ReviewList />
+      </RatingReview>
+    </>
   );
 }
--- client/src/Ratings-Reviews/RRcomponents/RatingSummary/RatingBreakdown/ratingBreakdownMain.jsx
+++ client/src/Ratings-Reviews/RRcomponents/RatingSummary/RatingBreakdown/ratingBreakdownMain.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import BarChart from './barChart.jsx';
 import RatingCount from './ratingCount.jsx';
 import StarLabels from './starLabels.jsx';
 
+const Breakdown =styled.div`
+display: flex;
+flex-flow: column wrap;
+align-self: flex-start;
+`;
+
 export default function RatingBreakdownMain() {
   return (
-      <div className="ratingsbreakdown head">ratingsbreakdown>
+      <Breakdown className="ratingsbreakdown main">ratingsbreakdown>
         <BarChart />
         <RatingCount />
         <StarLabels />
-      </div>
+      </Breakdown>
   );
 }
