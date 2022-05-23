4cac5766cc81197f2e40bf198f9269ea4902f92c
valpizzo <valpizzo2@gmail.com>
2022-05-23 19:45:59 -0400

    Formatted Related Item cards so they properly display live data, modal button changes color on hover, and created a file for helper function for the server.
    Inside the helper function right now is just a function that takes in an array of ratings and finds the average rating to be used to calculate stars.
    
    I am making axios calls inside my parent component for now, but eventually those calls should be moved to our server file.

--- client/src/RIOC/RelatedCard.jsx
+++ client/src/RIOC/RelatedCard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import axios from 'axios';
 import CardDiv from './RIOC-styled-components/CardDiv';
+import StarRating from '../shared/StarRating';
+import ModalButton from './RIOC-styled-components/ModalButton';
+import '../../dist/styles/styles-val.css';
 
-function RelatedCard({ cardNum }) {
+function RelatedCard({ cardProduct, cardStyle, cardRating }) {
+  const productImage = cardStyle ? cardStyle.results[0].photos[0].thumbnail_url : '';
   return (
     <CardDiv onClick={() => { console.log('Should go to product page'); }}>
-      <p>Card</p>
-      <p>{cardNum}</p>
+      <div className="prodImg" style={{ backgroundImage: `url(${productImage})` }}>
+        <ModalButton type="button">⭐️</ModalButton>
+      </div>
+      <p>{cardProduct.category}</p>
+      <h6>{cardProduct.name}</h6>
+      <p>{cardProduct.default_price}</p>
+      <StarRating rating={cardRating} />
     </CardDiv>
   );
 }
