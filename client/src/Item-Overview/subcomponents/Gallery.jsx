f57ffed17365d882fd6ad00438a00b262c3a4d1b
Davyd Zakorchennyi <Diza41a@mail.com>
2022-05-24 02:05:01 -0400

    Save for the night

--- client/src/Item-Overview/subcomponents/Gallery.jsx
+++ client/src/Item-Overview/subcomponents/Gallery.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Gallery(props) {
   const {
@@ -10,11 +10,14 @@ function Gallery(props) {
   const [imageIndex, setImageIndex] = useState(0);
   const { photos } = styles[styleIndex];
 
+  // Allow for only up to 4 thumbnails at a time
+  const startIndex = Math.floor(imageIndex / 4) * 4;
+  const endIndex = startIndex + 3 < photos.length ? startIndex + 3 : photos.length - 1;
+
   const thumbnails = [];
-  for (let i = 0; i < photos.length; i += 1) {
+  for (let i = startIndex; i <= endIndex; i += 1) {
     const css = { backgroundImage: `url(${photos[i]?.thumbnail_url})` };
     const classList = i !== imageIndex ? 'thumbnail' : 'thumbnail thumbnail-selected';
-
     thumbnails.push((
       <li>
         <button type="button" className={classList} style={css} data-index={i}> </button>
@@ -42,11 +45,18 @@ function Gallery(props) {
     e.preventDefault();
     if (e.target.classList.contains('thumbnail')) {
       const index = e.target.getAttribute('data-index');
+      // TODO: Bug fix -> tech debt ticket
+      document.querySelectorAll('[data-index]').forEach((thumbnail) => {
+        thumbnail.classList.remove('thumbnail-selected');
+        if (thumbnail.getAttribute('data-index') === index) {
+          thumbnail.classList.add('thumbnail-selected');
+        }
+      });
       setImageIndex(index);
     }
   };
 
-  const arrowClick = (e) => {
+  const arrowNavigationClick = (e) => {
     e.preventDefault();
     const action = e.target.getAttribute('data-action');
     if (action === 'imageNext') {
@@ -56,6 +66,24 @@ function Gallery(props) {
     } else if (action === 'imagePrev') {
       if (imageIndex - 1 >= 0) {
         setImageIndex(imageIndex - 1);
+      }
+    }
+  };
+
+  
+  const arrowThumbnailClick = (e) => {
+    e.preventDefault();
+    const action = e.target.getAttribute('data-action');
+    const page = Math.floor(imageIndex / 4) * 4;
+    if (action === 'carouselNext') {
+      if (page + 1 < photos.length && page !== 0) {
+        setImageIndex(page + 1);
+      } else if (page === 0 && page + 4 < photos.length) {
+        setImageIndex(page + 4);
+      }
+    } else if (action === 'carouselPrev') {
+      if (page > 0) {
+        setImageIndex(page - 4);
       }
     }
   };
@@ -74,8 +102,8 @@ function Gallery(props) {
         </div>
 
         <div className="image-list-wrapper">
-          <button type="button" className="gallery-up">
-            <i className="fa-solid fa-arrow-up" />
+          <button type="button" className="gallery-up" data-action="carouselPrev" onClick={arrowThumbnailClick}>
+            <i className="fa-solid fa-arrow-up" data-action="carouselPrev" />
           </button>
 
           <ul className="thumbnails" onClick={thumbnailClick}>
@@ -87,16 +115,16 @@ function Gallery(props) {
             ))}
           </ul>
 
-          <button type="button" className="gallery-up">
-            <i className="fa-solid fa-arrow-down" />
+          <button type="button" className="gallery-down" data-action="carouseNext" onClick={arrowThumbnailClick}>
+            <i className="fa-solid fa-arrow-down" data-action="carouselNext" />
           </button>
         </div>
 
         <div className="image-navigation">
-          <button type="button" className="gallery-left tall-arrows" data-action="imagePrev" onClick={arrowClick}>
+          <button type="button" className="gallery-left" data-action="imagePrev" onClick={arrowNavigationClick}>
             <i className="fa-solid fa-arrow-left" />
           </button>
-          <button type="button" className="gallery-right tall-arrows" data-action="imageNext" onClick={arrowClick}>
+          <button type="button" className="gallery-right" data-action="imageNext" onClick={arrowNavigationClick}>
             <i className="fa-solid fa-arrow-right" />
           </button>
         </div>
