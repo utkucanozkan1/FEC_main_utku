f3de1d20e4f84a761ad5840c7d53a5954a6f52b9
Utku Ozkan <utkucozkan@gmail.com>
2022-05-26 00:00:52 -0400

    added helpful button functionality to answers

--- client/src/q-a/QuestionList.jsx
+++ client/src/q-a/QuestionList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import ImageComponent from "./ImageComponent.jsx";
@@ -9,6 +10,7 @@ const QuestionList = function (props) {
   const [answerArr, setAnswerArr] = useState([]);
   const [loading, toogleLoading] = useState(true);
   const [answerReported , setAnswerReported] = useState(false);
+  const [answerHelpful , setAnswerHelpful] = useState(false);
   // console.log("answerArr:", answerArr);
   // answerArray = Object.keys(props.question.answers)
   // for(let key in props.question.answers){
@@ -26,6 +28,7 @@ const QuestionList = function (props) {
         if (result.data.results) {
           setAnswerArr(result.data.results);
           toogleLoading(false);
+          setAnswerHelpful(false);
         }
       })
       .then()
@@ -38,6 +41,13 @@ const QuestionList = function (props) {
     axios.put(`/answer/report/${answerId}`)
       .then(setAnswerReported(true))
       .then((res) => console.log('answer reported'))
+      .catch((err) => console.log(err));
+  }
+  function helpfulAnswer(event) {
+    let answerId = answerArr[0].answer_id;
+    axios.put(`/answer/helpful/${answerId}`)
+      .then(setAnswerHelpful(true))
+      .then((res) => console.log('answer helpful'))
       .catch((err) => console.log(err));
   }
 
@@ -59,11 +69,11 @@ const QuestionList = function (props) {
             <b>Q: {props.question.question_body}</b>
           </div>
           <div className="flex-child">
-            <button className="astext-btn">
+            <button type="button" className="astext-btn">
               {ButtonTitle} ({props.question.question_helpfulness})
             </button>
             &nbsp; &nbsp; | &nbsp; &nbsp;
-            <button className="astext-btn-answer">Add Answer</button>
+            <button type="button" className="astext-btn-answer">Add Answer</button>
           </div>
         </div>
         <div>
@@ -80,20 +90,37 @@ const QuestionList = function (props) {
         <div>
           <span style={{ fontSize: "small" }}>
             by &nbsp;
-            {answerArr[0].answerer_name}, &nbsp;
+            {answerArr[0].answerer_name}
+            ,
+                  &nbsp;
             {moment(answerArr[0].date.slice(0, 10)).format("MMM Do YY")}
           </span>
           &nbsp; &nbsp; | &nbsp; &nbsp;
-          <button type="button" className="astext-btn">
+          {answerHelpful ? <button type="button" className="astext-btn">This Answer has been marked Helpful</button> : (
+            <button type="button" className="astext-btn" onClick={(event) => { helpfulAnswer(event); }}>
+              <text style={{ fontSize: 'small' }}>Helpful?</text>
+                    &nbsp;
+              <mark style={{ textDecoration: 'underline', fontSize: 'small' }}>
+                Yes
+              </mark>
+              (
+              {answerArr[0].helpfulness}
+              )
+            </button>
+          )}
+          {/* <button type="button" className="astext-btn">
             {ButtonTitle} ({answerArr[0].helpfulness})
-          </button>
+          </button> */}
           &nbsp; &nbsp; | &nbsp; &nbsp;
-          {answerReported ? <button className="astext-btn-answer">This Answer has been Reported</button> :  <button type="button" className="astext-btn-answer" onClick={reportAnswer}>
-            Report
-          </button>}
+          {answerReported ? <button type="button" className="astext-btn-answer" >This Answer has been Reported</button> : (
+            <button type="button" className="astext-btn-answer" onClick= {reportAnswer}>
+              Report
+            </button>
+          )}
         </div>
       </div>
     );
+  // eslint-disable-next-line no-else-return
   } else {
     <section>
       <div> Questions Loading...</div>
