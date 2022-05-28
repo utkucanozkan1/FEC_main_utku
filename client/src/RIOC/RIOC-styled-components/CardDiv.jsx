2b6fac2c8eba7865777553ac2aa0e9e7a18b6a9d
valpizzo <valpizzo2@gmail.com>
2022-05-28 17:41:50 -0400

    Preparing to pull changes

--- client/src/RIOC/RIOC-styled-components/CardDiv.jsx
+++ client/src/RIOC/RIOC-styled-components/CardDiv.jsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 const CardDiv = styled.div`
   border-style: solid;
   border-color: black;
-  width: 150px;
-  min-width: 25px;
-  max-width: 100px;
-  min-height: 150px;
+  flex: 1 1 0px;
+  height: 250px;
   margin: 5px;
   white-space: nowrap;
   overflow: hidden;
